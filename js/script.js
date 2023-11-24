ef03ce87702addf58e942a0fc0b61f55fe3b2170
Gilkleyton Rodrigues <gilkleyton.andrade@gmail.com>
2023-11-24 18:17:32 -0300

    alteracao no mobile

--- js/script.js
+++ js/script.js
@@ -1,15 +1,7 @@
-var togglebtn=document.querySelector(".togglebtn");
-        var nav=document.querySelector(".navlinks");
-        var links=document.querySelector(".navlinks li");
 
-        togglebtn.addEventListener("click", function(){
-            this.classList.toggle("click");
-            nav.classList.toggle("open");
-        })
-
-        var typed=new Typed(".input",{
-            strings:["Frontend Developer","Web Developer"],
-            typedSpeed:70,
-            backSpeed:55,
-            loop:true
-        })
+var typed=new Typed(".input",{
+    strings:["Frontend Developer","Web Developer"],
+    typedSpeed:70,
+    backSpeed:55,
+    loop:true
+});
