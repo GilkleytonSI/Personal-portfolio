dfe65646c3a65bcacd5f6057a7cf06aff7f9d4b8
Gilkleyton Rodrigues <gilkleyton.andrade@gmail.com>
2024-07-19 01:58:30 -0300

    adcionei projetos, e alterei na estrutura do site

--- js/script.js
+++ js/script.js
@@ -1,3 +1,26 @@
+const modal = document.getElementById("modal");
+    const modalImg = document.getElementById("modal-img");
+    const images = document.querySelectorAll(".projeto img");
+    const closeModal = document.getElementsByClassName("close")[0];
+
+    images.forEach(img => {
+        img.addEventListener("click", function() {
+            modal.style.display = "block";
+            modalImg.src = this.src;
+        });
+    });
+
+    closeModal.addEventListener("click", function() {
+        modal.style.display = "none";
+    });
+
+    window.addEventListener("click", function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    });
+
+
 
 var typed=new Typed(".input",{
     strings:["Frontend Developer","Web Developer"],
@@ -5,3 +28,5 @@ var typed=new Typed(".input",{
     backSpeed:55,
     loop:true
 });
+
+    
